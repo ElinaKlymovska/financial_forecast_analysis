3ac592763a20faaaf53446ebe87b66aa37a20c49
ElinaKlymovska <klymovska.elina@gmail.com>
2025-01-27 19:41:09 +0100

    provided analyzer articles

--- src/ingestion/data_ingestion.py
+++ src/ingestion/data_ingestion.py
@@ -1,10 +1,11 @@
+from typing import List
 from src.ingestion.data_reception.news_from_news import NewsAPIDataSource
-from src.ingestion.data_reception.сrypto_сompare import CryptoCompareDataSource
+from src.ingestion.data_reception.сrypto_сompare import CryptoCompareDataSource, NormalizedData
 
 
-def ingest_data(query=None, exclude_categories=None):
+def ingest_data(query=None, exclude_categories=None) -> List[NormalizedData]:
     """
-    Завантаження даних з усіх джерел з врахуванням фільтрів.
+    Load data from all sources with applied filters.
     """
     crypto_source = CryptoCompareDataSource()
     news_source = NewsAPIDataSource()
@@ -12,8 +13,5 @@ def ingest_data(query=None, exclude_categories=None):
     crypto_data = crypto_source.fetch_data(query=query, exclude_categories=exclude_categories)
     news_data = news_source.fetch_data(query=query, exclude_categories=exclude_categories)
 
-    combined_data = crypto_data + news_data
+    combined_data: List[NormalizedData] = crypto_data + news_data
     return combined_data
-
-
-
